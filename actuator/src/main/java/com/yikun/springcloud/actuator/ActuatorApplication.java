6b201b52c6965668142746a9597c7a4c79075109
caiweikun <weikun@yikun.info>
2019-10-23 17:27:12 +0800

    first commit actuator modify

--- actuator/src/main/java/com/yikun/springcloud/actuator/ActuatorApplication.java
+++ actuator/src/main/java/com/yikun/springcloud/actuator/ActuatorApplication.java
@@ -1,13 +1,24 @@
 package com.yikun.springcloud.actuator;
+import	java.io.Console;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class ActuatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String test(){
+        return "hello";
     }
 
 }
