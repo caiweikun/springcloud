d89356b3cd081864c25845ff232326e5034ada33
caiweikun <weikun@yikun.info>
2019-10-23 17:12:42 +0800

    first commit actuator

---
+++ actuator/src/main/java/com/yikun/springcloud/actuator/ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.yikun.springcloud.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+}
---
+++ actuator/src/test/java/com/yikun/springcloud/actuator/ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yikun.springcloud.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
