5571ddb8be92242b8cbe71f339ca79b3f8d794aa
caiweikun <weikun@yikun.info>
2019-10-24 17:31:48 +0800

    eureka init

---
+++ eureka/src/test/java/com/yikun/springcloud/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yikun.springcloud.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
