2f26abd2347688b604d2769a750de37024b63b0a
caiweikun <weikun@yikun.info>
2019-10-26 11:47:49 +0800

    commit config bus

--- ribbon/eureka-config-client/src/main/java/com/yikun/springcloud/eurekaconfigclient/EurekaConfigClientApplication.java
+++ ribbon/eureka-config-client/src/main/java/com/yikun/springcloud/eurekaconfigclient/EurekaConfigClientApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 使用 Spring Cloud Bus 刷新配置
+ */
 @EnableEurekaClient
 @SpringBootApplication
 public class EurekaConfigClientApplication {
--- ribbon/eureka-config-client/src/main/java/com/yikun/springcloud/eurekaconfigclient/controller/SelfController.java
+++ ribbon/eureka-config-client/src/main/java/com/yikun/springcloud/eurekaconfigclient/controller/SelfController.java
@@ -1,10 +1,13 @@
 package com.yikun.springcloud.eurekaconfigclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+// spring cloud bus 更新配置
+@RefreshScope
 public class SelfController {
 
     @Value("${self.name}")
@@ -12,8 +15,16 @@ public class SelfController {
     @Value("${self.age}")
     private Integer age;
 
+    @Value("${self.code}")
+    private String code;
+
     @GetMapping("/self")
     public String selfInfo(){
         return "name:"+name+",age:"+age;
+    }
+
+    @GetMapping("/self/code")
+    public String selfInfoCode(){
+        return "code:"+code;
     }
 }
